503881f7c3f864007f4bd1be08b937f1cd815e73
Ayoube <121962148+AyoubeM@users.noreply.github.com>
2024-05-05 13:20:20 +0200

    ajout du lecteur de selection de chapitre des premiere image ajout de toute les page de 1 a 14 un chapitre special et creation d'un nv css et de script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script pour gérer la redirection vers le chapitre sélectionné
+document.getElementById('chapitre').addEventListener('change', function() {
+    window.location.href = this.value;
+  });
+  
